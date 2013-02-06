2b608fdf47ed55f938e72f92b886fe2cc4409e1f
Arthur Ketcham <arthur@kaskadia.com>
2013-02-06 00:59:28 -0500

    Initial Commit

--- README.md
+++
@@ -1,4 +0,0 @@
-subtitlet
-=========
-
-A novel and fun captcha test that provides subtitles to independent film-makers.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subtitlet::Application.config.secret_token = '68647ced225e46579bae783fedcd47f04a7edeee16087a739b8df5a6ee684d175b28274ad13b73bfe7c2f0f11bb4eced2f49cb9dff91b48371a9a1ce7f76c209'
